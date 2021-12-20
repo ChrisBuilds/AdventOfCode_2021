d4a280b4e4d23e393074b35955c586364083b579
Chris Holland <741258+github@pm.me>
2021-12-19 20:09:42 -0500

    Working on string handling solution

--- dec18/dec18-1.py
+++ dec18/dec18-1.py
@@ -5,7 +5,7 @@ with open("input.txt") as f:
 def explode_check(sn_str):
     max_depth = 0
     depth = 0
-    for char in snail_num:
+    for char in sn_str:
         if char == "[":
             depth += 1
             if depth > max_depth:
@@ -27,18 +27,32 @@ def split_check(sn_list):
     return split_found
 
 
+def explode(sn_str):
+    depth = 0
+    global explode_left
+    global explode_right
+    for i, char in enumerate(sn_str):
+        if char == "[":
+            depth += 1
+        if depth == 5:
+            explode_left, explode_right = sn_str[i + 1 : i + 4].split(",")
+            left_str = sn_str[:i][::-1]
+            right_str = sn_str[i + 6 :]
+            break
+    print()
+
+
 def reduce_snail_num(sn_str, sn_list):
     pending_explode = explode_check(sn_str)
     while pending_explode:
         if pending_explode:
-            
-
-
+            explode(sn_str)
 
     pending_split = split_check(sn_list)
 
 
-
+explode_left = None
+explode_right = None
 for snail_num in data:
     sn_str = snail_num
     sn_list = eval(snail_num)
